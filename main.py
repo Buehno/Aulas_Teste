32545a0dc4d28bfb1f36cf11539564f55f7a61a1
Buehno <ronalo.rodrigues.dias.bueeno@gmail.com>
2024-05-29 14:40:15 -0300

    ajuste de rotas e organização dos arquivos e classes

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template , redirect , request , session, flash, url_for
 from flask_sqlalchemy import SQLAlchemy
 from views import *
-#classe para cadastrar o candidato
+
 
 app = Flask(__name__)
 
 app.config.from_pyfile('config.py')
 
-app.secret_key = 'aulastestsfecaf'
+
 db = SQLAlchemy(app)
 if __name__ == '__main__':
     app.run(debug=True)
